3e17ceebf1fe59be0c2164f6a16cf7f1a7db0c9b
MPerilly <msp501@nyu.edu>
2020-07-18 20:23:14 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/BAProg5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BAProg5.exe"
+  "BAProg5.exe.manifest"
+  "BAProg5.lib"
+  "BAProg5.pdb"
+  "CMakeFiles/BAProg5.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BAProg5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
